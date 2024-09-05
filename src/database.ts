5125a13cead62ae8511e91edbc60b3c1401fe64c
Guilherme  Rodrigues <35945894-silvaglrodrigue@users.noreply.replit.com>
2024-09-05 00:40:43 +0000

    chore: Incluir uma rota adicional no servidor para adicionar dados ao banco de dados

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import { Pool } from 'pg';
+
+// Substitua pela sua string de conexão do Render.com
+const connectionString = "postgresql://guilherme4na_user:uLcHu5CpGSY1udOy3uq4p6dbxuH1NKvj@dpg-cr7r02jtq21c739eiq90-a.oregon-postgres.render.com/guilherme4na";
+
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false, // Permite conexões SSL não autorizadas
+  }
+});
+
+export default pool;
