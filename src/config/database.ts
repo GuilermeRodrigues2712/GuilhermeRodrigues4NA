d75c53aab1f72fc88df0af739f3beb6bbf2d96c3
Guilherme  Rodrigues <35945894-silvaglrodrigue@users.noreply.replit.com>
2024-09-18 22:06:27 +0000

    feat: modulariza o projeto para separá-lo em rotas, controllers e a configuração do banco de dados, proporcionando uma organização mais clara e facilitando a manutenção

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 // Substitua pela sua string de conexão do Render.com
-const connectionString = "postgresql://guilherme4na_user:uLcHu5CpGSY1udOy3uq4p6dbxuH1NKvj@dpg-cr7r02jtq21c739eiq90-a.oregon-postgres.render.com/guilherme4na";
+const connectionString = 'postgresql://guilherme4na_user:uLcHu5CpGSY1udOy3uq4p6dbxuH1NKvj@dpg-cr7r02jtq21c739eiq90-a.oregon-postgres.render.com/guilherme4na';
 
 const pool = new Pool({
   connectionString,
